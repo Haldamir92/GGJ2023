bbcb5445e8f5a9286b2c36e64827a79673800b87
Giovanni Giampiccolo <giovanni.giampiccolo.92@gmail.com>
2023-02-05 12:27:49 +0100

    Add player health and game over condition event

--- Assets/_Project/Scripts/Player/PlayerStats.cs
+++ Assets/_Project/Scripts/Player/PlayerStats.cs
@@ -2,18 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityAtoms.BaseAtoms;
+using UnityEngine.Events;
 
 public class PlayerStats : MonoBehaviour
 {
+    [SerializeField]
+    private int health = 3;
+
     [SerializeField]
     private GameObjectValueList players;
 
     [SerializeField]
     private BoolVariableInstancer isPlayerInsideCameraBounds;
 
+    [SerializeField]
+    private UnityEvent onGameOver;
+
     private void Update()
     {
-        if(!isPlayerInsideCameraBounds.Value)
+        if (!isPlayerInsideCameraBounds.Value)
         {
             Die();
         }
@@ -26,21 +33,43 @@ public class PlayerStats : MonoBehaviour
 
     private void Respawn()
     {
-        int randomIndex = Random.Range(0, players.Count - 1);
+        List<PlayerStats> playersStatsList = new List<PlayerStats>();
 
-        for(int i = 0; i < players.Count; i++)
+        foreach (GameObject gObj in players.List)
         {
-            if (players[i] != gameObject)
+            PlayerStats ps = gObj.GetComponent<PlayerStats>();
+            if (ps != this && health > 0)
             {
-                if(randomIndex == 0)
-                {
-                    transform.position = players[i].transform.position;
-                }
-                else
-                {
-                    randomIndex--;
-                }
+                playersStatsList.Add(ps);
             }
         }
+        if (playersStatsList.Count > 0)
+        {
+            health--;
+            playersStatsList[0].health--;
+            playersStatsList.Shuffle();
+            transform.position = playersStatsList[0].transform.position;
+        }
+        else
+        {
+            onGameOver.Invoke();
+        }
+
+        //int randomIndex = Random.Range(0, players.Count - 1);
+
+        //for (int i = 0; i < players.Count; i++)
+        //{
+        //    if (players[i] != gameObject)
+        //    {
+        //        if (randomIndex == 0)
+        //        {
+        //            transform.position = players[i].transform.position;
+        //        }
+        //        else
+        //        {
+        //            randomIndex--;
+        //        }
+        //    }
+        //}
     }
 }
